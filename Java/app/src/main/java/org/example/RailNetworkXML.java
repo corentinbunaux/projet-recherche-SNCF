afe7c21f3fe61c26fcd7876106eb9fae051eb995
Corentin Bunaux <corentin.bunaux@gmail.com>
2025-03-07 18:47:18 +0100

    verteces ok

--- Java/app/src/main/java/org/example/RailNetworkXML.java
+++ Java/app/src/main/java/org/example/RailNetworkXML.java
@@ -18,12 +18,13 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseMultigraph;
-
+import java.awt.geom.Point2D;
 
 public class RailNetworkXML {
 
@@ -31,43 +32,70 @@ public class RailNetworkXML {
     public static final Map<String, String> attributesConditions = Map.of("CodeLocalisation", "FR", "CodeTypePr", "GARE");
 
     public static void filterData(String filePath) {
+        Document doc = readXMLFile(filePath);
+        Document filteredXML = createXMLFile();
+        try {
+            // Filter the data from the original XML and add to the new XML
+            NodeList nodeList = doc.getElementsByTagName("PR");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Element station = (Element) nodeList.item(i);
+                boolean matchesConditions = true;
+                for (Map.Entry<String, String> condition : attributesConditions.entrySet()) {
+                    String attributeValue = station.getAttribute(condition.getKey());
+                    if (!attributeValue.equals(condition.getValue())) {
+                        matchesConditions = false;
+                        break;
+                    }
+                }
+                if (matchesConditions) {
+                    Node importedNode = filteredXML.importNode(station, true);
+                    filteredXML.getDocumentElement().appendChild(importedNode);
+                }
+            }
+        } catch (DOMException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        writeDataToXMLFile(filteredXML, "data/rail_network.xml");
+    }
+
+    public static Document readXMLFile(String filePath) {
         try {
             File xmlFile = new File(filePath);
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(xmlFile);
             doc.getDocumentElement().normalize();
+            return doc;
+        } catch (IOException | ParserConfigurationException | DOMException | SAXException e) {
+            System.err.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
 
-            // Create a new XML document for the filtered data
+    public static Document createXMLFile(){
+        try {
             DocumentBuilderFactory dbFactory2 = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder2 = dbFactory2.newDocumentBuilder();
             Document filteredXML = dBuilder2.newDocument();
             Element rootElement = filteredXML.createElement("rail_network");
             filteredXML.appendChild(rootElement);
+            return filteredXML;
+        } catch (ParserConfigurationException e) {
+            System.err.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
 
-            // Filter the data from the original XML and add to the new XML
-            NodeList nodeList = doc.getElementsByTagName("PR");
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Element station = (Element) nodeList.item(i);
-                for (String attribute : stationsAttributesToKeep) {
-                    String attributeValue = station.getAttribute(attribute);
-                    if (attributesConditions.containsKey(attribute) && !attributeValue.equals(attributesConditions.get(attribute))) {
-                        continue;
-                    }
-                    Element newStation = filteredXML.createElement(attribute);
-                    newStation.appendChild(filteredXML.createTextNode(attributeValue));
-                    rootElement.appendChild(newStation);
-                }
-            }
-
+    public static void writeDataToXMLFile(Document doc, String filePath) {
+        try {
             // Write the new XML content to a file
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(filteredXML);
-            StreamResult result = new StreamResult(new File("data/rail_network.xml"));
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(filePath));
             transformer.transform(source, result);
-        } catch (IOException | ParserConfigurationException | TransformerException | DOMException | SAXException e) {
-           System.err.println("Error: " + e.getMessage());
+        } catch (TransformerException e) {
+            System.err.println("Error: " + e.getMessage());
         }
     }
 
@@ -80,27 +108,74 @@ public class RailNetworkXML {
         }
     }
 
-    public static Graph<String, String> createRailNetwork(){
+        public static Graph<String, String> createRailNetwork(Map<String, Point2D> positions) {
         if (!isDataFiltered("data/rail_network.xml")) {
             System.out.println("Filtering data...");
             filterData("data/xml_REF_PROD_S01.xml");
         }
         Graph<String, String> railNetwork = new SparseMultigraph<>();
         try {
-            System.out.println("Creating rail network...");
-            File xmlFile = new File("data/rail_network.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlFile);
-            doc.getDocumentElement().normalize();
-
-            NodeList nodeList = doc.getElementsByTagName("CodeReseau");
+            Document doc = readXMLFile("data/rail_network.xml");
+            NodeList nodeList = doc.getElementsByTagName("PR");
+            System.out.println(nodeList.getLength() + " stations found in the rail network.");
+    
+            // Calculate min and max values for x and y coordinates
+            double minX = Double.MAX_VALUE, maxX = Double.MIN_VALUE;
+            double minY = Double.MAX_VALUE, maxY = Double.MIN_VALUE;
+    
             for (int i = 0; i < nodeList.getLength(); i++) {
                 Element station = (Element) nodeList.item(i);
-                String stationName = station.getTextContent();
-                railNetwork.addVertex(stationName);
+                String xStr = station.getAttribute("Longitude");
+                String yStr = station.getAttribute("Latitude");
+    
+                if (!xStr.isEmpty() && !yStr.isEmpty()) {
+                    double x = Double.parseDouble(xStr);
+                    double y = Double.parseDouble(yStr);
+    
+                    if (x < minX) {
+                        minX = x;
+                    }
+                    if (x > maxX) {
+                        maxX = x;
+                    }
+                    if (y < minY) {
+                        minY = y;
+                    }
+                    if (y > maxY) {
+                        maxY = y;
+                    }
+    
+                    railNetwork.addVertex(station.getAttribute("Libelle"));
+                }
             }
-        } catch (IOException | ParserConfigurationException | DOMException | SAXException e) {
+    
+            // Add padding to min and max values
+            double paddingX = (maxX - minX) * Window.margin;
+            double paddingY = (maxY - minY) * Window.margin;
+            minX -= paddingX;
+            maxX += paddingX;
+            minY -= paddingY;
+            maxY += paddingY;
+    
+            // Normalize positions for display
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Element station = (Element) nodeList.item(i);
+                String xStr = station.getAttribute("Longitude");
+                String yStr = station.getAttribute("Latitude");
+    
+                if (!xStr.isEmpty() && !yStr.isEmpty()) {
+                    double x = Double.parseDouble(xStr);
+                    double y = Double.parseDouble(yStr);
+                    double normX = (x - minX) * Window.height / (maxX - minX);
+                    double normY = (y - minY) * Window.height / (maxY - minY);
+                    double symY = Window.height - normY;
+                    positions.put(station.getAttribute("Libelle"), new Point2D.Double(normX, symY));
+                }
+            }
+    
+            System.out.println(railNetwork.getVertexCount() + " stations added to the rail network.");
+    
+        } catch (DOMException e) {
             System.err.println("Error: " + e.getMessage());
         }
         System.out.println("Rail network created.");
