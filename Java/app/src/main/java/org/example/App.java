083fd36ef46f65e897de2c50335525565e9990e7
Corentin Bunaux <corentin.bunaux@gmail.com>
2025-03-10 14:29:46 +0100

    ...

--- Java/app/src/main/java/org/example/App.java
+++ Java/app/src/main/java/org/example/App.java
@@ -1,10 +1,16 @@
 package org.example;
 
+<<<<<<< Updated upstream
 
 import java.awt.geom.Point2D;
 import java.util.HashMap;
 import java.util.Map;
 
+=======
+import java.awt.geom.Point2D;
+import java.util.HashMap;
+import java.util.Map;
+>>>>>>> Stashed changes
 import com.formdev.flatlaf.FlatLightLaf;
 
 import edu.uci.ics.jung.graph.Graph;
@@ -15,6 +21,10 @@ public class App {
         Map<String, Point2D> positions = new HashMap<>();
         Graph<String, String> railNetwork = RailNetwork.createRailNetwork(positions);
         GUI.display(railNetwork, positions);
+<<<<<<< Updated upstream
         // Graph<String, String> railNetworkXML = RailNetworkXML.createRailNetwork();
+=======
+        //Graph<String, String> railNetwork = RailNetworkXML.createRailNetwork();
+>>>>>>> Stashed changes
     }
 }
